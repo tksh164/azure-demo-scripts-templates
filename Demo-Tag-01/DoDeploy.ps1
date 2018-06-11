9865555791a26a6786f2ee14f050a765132265a3
Takeshi Katano <takatano@outlook.com>
2018-06-11 22:50:26 +0900

    Add ARM template for tag feature demonstration

---
+++ Demo-Tag-01/DoDeploy.ps1
@@ -0,0 +1,2 @@
+.\Demo-Tag-Test\deploy.ps1 -ResourceGroupName 'Demo-Tag-Test-RG' -ResourceGroupLocation 'Japan East'
+.\Demo-Tag-Prod\deploy.ps1 -ResourceGroupName 'Demo-Tag-Prod-RG' -ResourceGroupLocation 'Japan East'
