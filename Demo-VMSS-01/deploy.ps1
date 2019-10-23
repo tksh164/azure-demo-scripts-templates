a2d61517c2fa5932736df869df649eff66adde3a
Takeshi Katano <takatano@outlook.com>
2019-10-24 08:55:49 +0900

    Change PowerShell module from AzureRM to Az

--- Demo-VMSS-01/deploy.ps1
+++ Demo-VMSS-01/deploy.ps1
@@ -1,7 +1,6 @@
 #Requires -Version 3.0
-#Requires -Module AzureRM.Profile
-#Requires -Module AzureRM.Resources
-#Requires -Module Azure.Storage
+#Requires -Module Az.Accounts
+#Requires -Module Az.Resources
 
 Param(
     [string] $ResourceGroupName = 'Demo-VMSS-RG',
@@ -20,7 +19,7 @@ function Format-ValidationOutput {
     return @($ValidationOutput | Where-Object { $_ -ne $null } | ForEach-Object { @('  ' * $Depth + ': ' + $_.Message) + @(Format-ValidationOutput @($_.Details) ($Depth + 1)) })
 }
 
-Get-AzureRmContext
+Get-AzContext
 
 $OptionalParameters = New-Object -TypeName Hashtable
 $TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateFile))
@@ -28,13 +27,13 @@ $TemplateParametersFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combin
 
 
 # Create or update the resource group using the specified template file and template parameters file
-New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Tag @{ 'Usage' = 'Demo' } -Verbose -Force
+New-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Tag @{ 'Usage' = 'Demo' } -Verbose -Force
 
 if ($ValidateOnly) {
-    $ErrorMessages = Format-ValidationOutput (Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
-                                                                                  -TemplateFile $TemplateFile `
-                                                                                  -TemplateParameterFile $TemplateParametersFile `
-                                                                                  @OptionalParameters)
+    $ErrorMessages = Format-ValidationOutput (Test-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
+                                                                             -TemplateFile $TemplateFile `
+                                                                             -TemplateParameterFile $TemplateParametersFile `
+                                                                             @OptionalParameters)
     if ($ErrorMessages) {
         Write-Output '', 'Validation returned the following errors:', @($ErrorMessages), '', 'Template is invalid.'
     }
@@ -43,13 +42,13 @@ if ($ValidateOnly) {
     }
 }
 else {
-    New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
-                                       -ResourceGroupName $ResourceGroupName `
-                                       -TemplateFile $TemplateFile `
-                                       -TemplateParameterFile $TemplateParametersFile `
-                                       @OptionalParameters `
-                                       -Force -Verbose `
-                                       -ErrorVariable ErrorMessages
+    New-AzResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+                                  -ResourceGroupName $ResourceGroupName `
+                                  -TemplateFile $TemplateFile `
+                                  -TemplateParameterFile $TemplateParametersFile `
+                                  @OptionalParameters `
+                                  -Force -Verbose `
+                                  -ErrorVariable ErrorMessages
     if ($ErrorMessages) {
         Write-Output '', 'Template deployment returned the following errors:', @(@($ErrorMessages) | ForEach-Object { $_.Exception.Message.TrimEnd("`r`n") })
     }
