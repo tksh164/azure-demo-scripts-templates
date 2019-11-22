01ee47e9ae75aadff83a6db199061eaf6f6622bd
Takeshi Katano <takatano@outlook.com>
2019-11-22 15:32:14 +0900

    Add on-premises to VNet demo

---
+++ Demo-OnPremToVNet-VPN/README.md
@@ -0,0 +1,36 @@
+# On-premises to VNet for demo
+
+The on-premises network is simulated by VNet.
+
+## Template overview
+
+### Deployment
+
+- VNet: `onprem-vnet`
+    - Subnet: `default`
+        - Virtual machine: `onprem-vm1`
+            - Disk: `onprem-vm1-osdisk`
+            - Network interface: `onprem-vm1-nic`
+            - Public IP address: `onprem-vm1-ip`
+            - Network security group: `onprem-vm1-nsg`
+    - Subnet: `GatewaySubnet`
+        - VPN gateway: `onprem-vpngw`
+            - Public IP address: `onprem-vpngw-ip` - For VPN gateway.
+- VNet: `azure-vnet`
+    - Subnet: `default`
+        - Virtual machine: `azure-vm1`
+            - Disk: `azure-vm1-osdisk`
+            - Network interface: `azure-vm1-nic`
+            - Public IP address: `azure-vm1-ip`
+            - Network security group: `azure-vm1-nsg`
+    - Subnet: `GatewaySubnet`
+        - VPN gateway: `azure-vpngw`
+            - Public IP address: `azure-vpngw-ip` - For VPN gateway.
+
+### No deployment
+
+- Create no connections inter VPN gateways.
+
+## Notes
+
+- The deploy.ps1 script needs [Az module](https://www.powershellgallery.com/packages/Az/).
---
+++ Demo-OnPremToVNet-VPN/deploy.ps1
@@ -0,0 +1,68 @@
+param (
+    [string] $ResourceGroupName = 'Demo-VPN-OnPrem-VNet',
+    [string] $ResourceGroupLocation = 'Japan East',
+    [string] $TemplateFile = 'template.json',
+    [string] $TemplateParametersFile = 'parameters.json',
+    [switch] $ValidateOnly
+)
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 3
+
+function Format-ValidationOutput
+{
+    param (
+        $ValidationOutput,
+        [int] $Depth = 0
+    )
+
+    Set-StrictMode -Off
+    return @($ValidationOutput |
+        Where-Object { $_ -ne $null } |
+        ForEach-Object { @('  ' * $Depth + ': ' + $_.Message) + @(Format-ValidationOutput @($_.Details) ($Depth + 1)) })
+}
+
+Get-AzContext
+
+$OptionalParameters = New-Object -TypeName Hashtable
+$TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateFile))
+$TemplateParametersFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile))
+
+New-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Tag @{ 'Usage' = 'Demo' } -Verbose -Force
+
+if ($ValidateOnly)
+{
+    $params = @{
+        ResourceGroupName     = $ResourceGroupName
+        TemplateFile          = $TemplateFile
+        TemplateParameterFile = $TemplateParametersFile
+    }
+    $errorMessages = Format-ValidationOutput (Test-AzResourceGroupDeployment @params @OptionalParameters)
+
+    if ($errorMessages)
+    {
+        Write-Output '', 'Validation returned the following errors:', @($errorMessages), '', 'Template is invalid.'
+    }
+    else
+    {
+        Write-Output '', 'Template is valid.'
+    }
+}
+else
+{
+    $params = @{
+        Name                  = ('{0}-{1}'-f (Get-ChildItem -LiteralPath $TemplateFile).BaseName, (Get-Date).ToUniversalTime().ToString('MMdd-HHmm'))
+        ResourceGroupName     = $ResourceGroupName
+        TemplateFile          = $TemplateFile
+        TemplateParameterFile = $TemplateParametersFile
+        Force                 = $true
+        Verbose               = $true
+        ErrorVariable         = 'errorMessages'
+    }
+    New-AzResourceGroupDeployment @params @OptionalParameters
+
+    if ($errorMessages)
+    {
+        Write-Output '', 'Template deployment returned the following errors:', @(@($errorMessages) | ForEach-Object { $_.Exception.Message.TrimEnd("`r`n") })
+    }
+}
