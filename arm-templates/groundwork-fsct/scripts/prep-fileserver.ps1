787d44a5daa454d5fa2d83799e3ca34dcb5911fc
Takeshi Katano <takatano@outlook.com>
2021-10-23 01:05:43 +0900

    Add scripts

---
+++ arm-templates/groundwork-fsct/scripts/prep-fileserver.ps1
@@ -0,0 +1,1 @@
+Install-WindowsFeature -Name 'FS-FileServer' -IncludeManagementTools -Restart
---
+++ arm-templates/groundwork-fsct/scripts/prep-fsct.ps1
@@ -0,0 +1,3 @@
+# Download the File Server Capacity Tool
+# https://www.microsoft.com/en-us/download/details.aspx?id=55947
+curl.exe -Lo 'C:\FSCT-V1.3-64bit.zip' 'https://download.microsoft.com/download/C/B/C/CBC1FB9E-6809-4B30-B81F-D279C63BEBDC/V1.3-64bit.zip'
