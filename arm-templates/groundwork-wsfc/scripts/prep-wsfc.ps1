aed3ff189d20536019c985c6ee7c1091b2eccf70
Takeshi Katano <takatano@outlook.com>
2021-10-23 03:50:15 +0900

    Update script

--- arm-templates/groundwork-wsfc/scripts/prep-wsfc.ps1
+++ arm-templates/groundwork-wsfc/scripts/prep-wsfc.ps1
@@ -1,1 +1,15 @@
+$metadata = Invoke-RestMethod -Method Get -Headers @{ 'Metadata' = 'true' } -Uri 'http://169.254.169.254/metadata/instance?api-version=2021-02-01'
+if ($metadata.compute.platformFaultDomain -eq "0")
+{
+    $driveLetter = 'W'
+    $volumeLabel = 'Witness'
+
+    Get-Disk |
+        Where-Object -FilterScript { ($_.PartitionStyle -eq 'RAW') -and ($_.AllocatedSize -eq 0) -and ($_.NumberOfPartitions -eq 0) } |
+        Sort-Object -Property 'DiskNumber' |
+        Select-Object -First 1 |
+        Initialize-Disk -PartitionStyle GPT -PassThru |
+        New-Volume -FileSystem NTFS -DriveLetter $driveLetter -FriendlyName $volumeLabel
+}
+
 Install-WindowsFeature -Name 'Failover-Clustering' -IncludeManagementTools -Restart
