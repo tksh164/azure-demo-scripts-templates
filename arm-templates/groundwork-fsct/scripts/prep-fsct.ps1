991fce45ce7180d8d41c79148c76b5dcf0328808
Takeshi Katano <takatano@outlook.com>
2021-10-23 02:08:48 +0900

    Update script

--- arm-templates/groundwork-fsct/scripts/prep-fsct.ps1
+++ arm-templates/groundwork-fsct/scripts/prep-fsct.ps1
@@ -1,3 +1,12 @@
 # Download the File Server Capacity Tool
 # https://www.microsoft.com/en-us/download/details.aspx?id=55947
-curl.exe -Lo 'C:\FSCT-V1.3-64bit.zip' 'https://download.microsoft.com/download/C/B/C/CBC1FB9E-6809-4B30-B81F-D279C63BEBDC/V1.3-64bit.zip'
+
+$fsctDownloadUrl = 'https://download.microsoft.com/download/C/B/C/CBC1FB9E-6809-4B30-B81F-D279C63BEBDC/V1.3-64bit.zip'
+$fsctFilePath = 'C:\FSCT-V1.3-64bit.zip'
+
+curl.exe -Lo $fsctFilePath $fsctDownloadUrl
+
+Push-Location
+Set-Location -LiteralPath ([System.IO.Path]::GetDirectoryName($fsctFilePath))
+Expand-Archive -LiteralPath $fsctFilePath 
+Pop-Location
