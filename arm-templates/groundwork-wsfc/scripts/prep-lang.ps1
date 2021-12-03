4b88cfe82f35734ffa53bfd875019ebde419c6f9
Takeshi Katano <takatano@outlook.com>
2021-12-03 23:48:08 +0900

    Update prep script

--- arm-templates/groundwork-wsfc/scripts/prep-lang.ps1
+++ arm-templates/groundwork-wsfc/scripts/prep-lang.ps1
@@ -1,22 +1,24 @@
+$files = @(
+    [PSCustomObject] @{
+        Source      = 'https://raw.githubusercontent.com/tksh164/azure-demo-scripts-templates/master/arm-templates/groundwork-wsfc/scripts/lang/lang-step1.ps1'
+        Destination = 'C:\work\lang-step1.ps1'
+    },
+    [PSCustomObject] @{
+        Source      = 'https://raw.githubusercontent.com/tksh164/azure-demo-scripts-templates/master/arm-templates/groundwork-wsfc/scripts/lang/lang-step2.ps1'
+        Destination = 'C:\work\lang-step2.ps1'
+    }
+)
+
 # Windows Server 2019
 if ($PSVersionTable.BuildVersion.Build -eq 17763)
 {
-    New-Item -Path 'C:\work' -ItemType Directory -Force
-
-    @(
-        [PSCustomObject] @{
-            Source      = 'https://raw.githubusercontent.com/tksh164/azure-demo-scripts-templates/master/arm-templates/groundwork-wsfc/scripts/lang/lang-step1.ps1'
-            Destination = 'C:\work\lang-step1.ps1'
-        },
-        [PSCustomObject] @{
-            Source      = 'https://raw.githubusercontent.com/tksh164/azure-demo-scripts-templates/master/arm-templates/groundwork-wsfc/scripts/lang/lang-step2.ps1'
-            Destination = 'C:\work\lang-step2.ps1'
-        },
-        [PSCustomObject] @{
-            Source      = 'https://raw.githubusercontent.com/tksh164/azure-demo-scripts-templates/master/arm-templates/groundwork-wsfc/scripts/lang/lang-ws2019-jajp.psd1'
-            Destination = 'C:\work\lang-ws2019-jajp.psd1'
-        }
-    ) | ForEach-Object -Process {
-        Invoke-RestMethod -Method Get -Uri $_.Source -UseBasicParsing | Set-Content -LiteralPath $_.Destination -Force
+    $files += [PSCustomObject] @{
+        Source      = 'https://raw.githubusercontent.com/tksh164/azure-demo-scripts-templates/master/arm-templates/groundwork-wsfc/scripts/lang/lang-ws2019-jajp.psd1'
+        Destination = 'C:\work\lang-ws2019-jajp.psd1'
     }
+}
+
+New-Item -Path 'C:\work' -ItemType Directory -Force
+$files | ForEach-Object -Process {
+    Invoke-RestMethod -Method Get -Uri $_.Source -UseBasicParsing | Set-Content -LiteralPath $_.Destination -Force
 }
