45e6cf86ce74b1596779fc73c7f6ecf1bdbd623a
Takeshi Katano <takatano@outlook.com>
2021-12-03 23:35:32 +0900

    Rename lang scripts

--- arm-templates/groundwork-wsfc/scripts/prep-lang.ps1
+++ arm-templates/groundwork-wsfc/scripts/prep-lang.ps1
@@ -6,15 +6,15 @@ if ($PSVersionTable.BuildVersion.Build -eq 17763)
     @(
         [PSCustomObject] @{
             Source      = 'https://raw.githubusercontent.com/tksh164/azure-demo-scripts-templates/master/arm-templates/groundwork-wsfc/scripts/lang/lang-step1.ps1'
-            Destination = 'C:\work\lang-jajp-step1.ps1'
+            Destination = 'C:\work\lang-step1.ps1'
         },
         [PSCustomObject] @{
             Source      = 'https://raw.githubusercontent.com/tksh164/azure-demo-scripts-templates/master/arm-templates/groundwork-wsfc/scripts/lang/lang-step2.ps1'
-            Destination = 'C:\work\lang-jajp-step2.ps1'
+            Destination = 'C:\work\lang-step2.ps1'
         },
         [PSCustomObject] @{
             Source      = 'https://raw.githubusercontent.com/tksh164/azure-demo-scripts-templates/master/arm-templates/groundwork-wsfc/scripts/lang/lang-ws2019-jajp.psd1'
-            Destination = 'C:\work\lang-jajp-ws2019.psd1'
+            Destination = 'C:\work\lang-ws2019-jajp.psd1'
         }
     ) | ForEach-Object -Process {
         Invoke-RestMethod -Method Get -Uri $_.Source -UseBasicParsing | Set-Content -LiteralPath $_.Destination -Force
