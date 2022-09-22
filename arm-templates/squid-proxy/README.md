1ffd98cd2cdb91712180538b04d1e9129348e389
Takeshi Katano <takatano@outlook.com>
2022-09-23 03:46:55 +0900

    Add Squid proxy template

---
+++ arm-templates/squid-proxy/README.md
@@ -0,0 +1,22 @@
+# Deploy a proxy server using Squid
+
+[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#blade/Microsoft_Azure_CreateUIDef/CustomDeploymentBlade/uri/https%3A%2F%2Fraw.githubusercontent.com%2Ftksh164%2Fazure-demo-scripts-templates%2Fmaster%2Farm-templates%2Fsquid-proxy%2Ftemplate.json/uiFormDefinitionUri/https%3A%2F%2Fraw.githubusercontent.com%2Ftksh164%2Fazure-demo-scripts-templates%2Fmaster%2Farm-templates%2Fsquid-proxy%2Fuiform.json)
+
+## Template overview
+
+Deploy a proxy server virtual machine using Squid into an existing virtual network.
+
+### Deployment
+
+To be updated.
+
+## Notes
+
+- The private IP address assignment of the proxy server virtual machine has set as **Dynamic**. Change to **Static** if you needed.
+- The deploy.ps1 script needs [Az module](https://www.powershellgallery.com/packages/Az/).
+
+## TODO
+
+- [ ] Update README
+- [ ] Add Public IP address option
+- [ ] Add Availability Set / Availability Zone option
---
+++ arm-templates/squid-proxy/deploy.ps1
@@ -0,0 +1,65 @@
+[CmdletBinding()]
+param (
+    [string] $ResourceGroupName = 'exptl-squid-proxy',
+    [string] $ResourceGroupLocation = 'japaneast',
+    [string] $TemplateFile = './template.json',
+    [string] $TemplateParametersFile = './parameters.json',
+    [HashTable] $ResourceGroupTag = @{ 'usage' = 'experimental' },
+    [switch] $WhatIf,
+    [switch] $ValidateOnly
+)
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+Set-StrictMode -Version 3
+
+$TemplateFilePath = [IO.Path]::GetFullPath([IO.Path]::Combine($PSScriptRoot, $TemplateFile))
+$TemplateParametersFilePath = [IO.Path]::GetFullPath([IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile))
+
+Get-AzContext
+
+New-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Tag $ResourceGroupTag -Verbose -Force
+
+if ($ValidateOnly)
+{
+    $params = @{
+        ResourceGroupName = $ResourceGroupName
+        TemplateFile      = $TemplateFilePath
+    }
+
+    if (Test-Path -LiteralPath $TemplateParametersFilePath -PathType Leaf)
+    {
+        $params.TemplateParameterFile = $TemplateParametersFilePath
+    }
+
+    $result = Test-AzResourceGroupDeployment @params
+    if ($result.Count -eq 0)
+    {
+        Write-Output -InputObject '', 'Template is valid.'
+    }
+    else
+    {
+        $result    
+    }
+}
+else
+{
+    $params = @{
+        ResourceGroupName = $ResourceGroupName
+        TemplateFile      = $TemplateFilePath
+        WhatIf            = $WhatIf
+    }
+
+    if (Test-Path -LiteralPath $TemplateParametersFilePath -PathType Leaf)
+    {
+        $params.TemplateParameterFile = $TemplateParametersFilePath
+    }
+
+    if (-not $WhatIf)
+    {
+        $params.Name    = ('{0}-{1}'-f (Get-Item -LiteralPath $TemplateFilePath).BaseName, (Get-Date).ToUniversalTime().ToString('MMdd-HHmm'))
+        $params.Force   = $true
+        $params.Verbose = $true
+    }
+
+    New-AzResourceGroupDeployment @params
+}
