ea87aa3006487a425327b41503f09b78bda54dd3
Takeshi Katano <takatano@outlook.com>
2022-09-24 22:17:15 +0900

    Update

--- arm-templates/aks-on-windows-server-lab/dsc/helpers/dsc-config-for-nested-vm.ps1
+++ arm-templates/aks-on-windows-server-lab/dsc/helpers/dsc-config-for-nested-vm.ps1
@@ -1,38 +1,38 @@
-configuration AzSHCIHost
-{
+Configuration WsfcNodeConfig {
     Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
 
-    node localhost
-    {
+    Node 'localhost' {
         LocalConfigurationManager {
+            ConfigurationMode  = 'ApplyOnly'
             RebootNodeIfNeeded = $true
             ActionAfterReboot  = 'ContinueConfiguration'
-            ConfigurationMode = 'ApplyAndAutoCorrect'
-            ConfigurationModeFrequencyMins = 1440
         }
 
-        WindowsFeatureSet "AzSHCI Required Roles"
-        {
+        WindowsFeatureSet 'Install Windows roles and features' {
             Ensure = 'Present'
-            Name = @("File-Services", "FS-FileServer", "FS-Data-Deduplication", "BitLocker", "Data-Center-Bridging", "EnhancedStorage", "Failover-Clustering", "RSAT", "RSAT-Feature-Tools", "RSAT-DataCenterBridging-LLDP-Tools", "RSAT-Clustering", "RSAT-Clustering-PowerShell", "RSAT-Role-Tools", "RSAT-AD-Tools", "RSAT-AD-PowerShell", "RSAT-Hyper-V-Tools", "Hyper-V-PowerShell")
+            Name   = @(
+                'FS-FileServer',
+                'Failover-Clustering',
+                'RSAT-Clustering',
+                'Hyper-V',
+                'RSAT-Hyper-V-Tools'
+            )
         }
     }
 }
 
-$date = get-date -f yyyy-MM-dd
-$logFile = Join-Path -Path "C:\temp" -ChildPath $('AzSHCIHost-Transcipt-' + $date + '.log')
-$DscConfigLocation = "c:\temp\AzSHCIHost"
+$transcriptLogPath = Join-Path -Path 'C:\Temp' -ChildPath ('transcipt-{0}.log' -f (Get-Date -Format 'yyyy-MM-dd'))
+Start-Transcript -LiteralPath $transcriptLogPath
 
-Start-Transcript -Path $logFile
+$dscConfigLocation = 'C:\Temp\WsfcNodeConfig'
 
 Remove-DscConfigurationDocument -Stage Current, Previous, Pending -Force
 
-AzSHCIHost -OutputPath $DscConfigLocation 
+WsfcNodeConfig -OutputPath $dscConfigLocation
 
-Set-DscLocalConfigurationManager -Path $DscConfigLocation -Verbose
-
-Start-DscConfiguration -Path $DscConfigLocation -Wait -Verbose
+Set-DscLocalConfigurationManager -Path $dscConfigLocation -Verbose
+Start-DscConfiguration -Path $dscConfigLocation -Wait -Verbose
 
 Stop-Transcript
 
-Logoff
+logoff.exe
