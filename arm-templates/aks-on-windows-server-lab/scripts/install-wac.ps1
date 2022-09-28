b33383b7dc07682cfedfc68c63e25f80fc7a1fdc
Takeshi Katano <takatano@outlook.com>
2022-09-29 00:00:58 +0900

    Update

--- arm-templates/aks-on-windows-server-lab/scripts/install-wac.ps1
+++ arm-templates/aks-on-windows-server-lab/scripts/install-wac.ps1
@@ -1,10 +1,12 @@
 $ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
 
-New-Item -Path 'C:\WAC' -ItemType Directory -Force
+$wacFolderPath = 'C:\WAC'
+New-Item -Path $wacFolderPath -ItemType Directory -Force
 
 # Download the MSI file
-Start-BitsTransfer -Source 'https://aka.ms/WACDownload' -Destination 'C:\WAC\WindowsAdminCenter.msi'
+$wacMsiFilePath = [IO.Path]::Combine($wacFolderPath, 'WindowsAdminCenter.msi')
+Start-BitsTransfer -Source 'https://aka.ms/WACDownload' -Destination $wacMsiFilePath
 
 # Install Windows Admin Center
-$msiArgs = '/i', 'C:\WAC\WindowsAdminCenter.msi', '/qn', '/L*v', 'log.txt', 'SME_PORT=443', 'SSL_CERTIFICATE_OPTION=generate'
+$msiArgs = '/i', ('"{0}"' -f $wacMsiFilePath), '/qn', '/L*v', 'log.txt', 'SME_PORT=443', 'SSL_CERTIFICATE_OPTION=generate'
 Start-Process -FilePath 'msiexec.exe' -ArgumentList $msiArgs -Wait
